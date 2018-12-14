af11108cfe7adddb077ec0b7df3958d61487bbca
Mohamed Magdy <mohamed.moussaaly@student.guc.edu.eg>
2018-12-14 21:14:43 +0200

    removing code from index.php and make it a front end page to work in it later after login into the website (Landing page).

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php include('server.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,24 +8,5 @@
     <div class = "header">
         <h2>Home page</h2>
     </div>
-
-    <div class="content">
-        <?php if (isset($_SESSION['success'])): ?>
-            <div class ="error success">
-                <h3>
-                    <?php
-                        echo $_SESSION['success'];
-                        unset($_SESSION['success']);
-                    ?>
-                </h3>
-            </div>
-        <?php endif ?>
-
-        <?php if (isset($_SESSION["username"])): ?>
-            <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-            <p><a href="index.php?Logout='1'" style="color: red;">Logout</a></p>
-        <?php endif ?>
-    </div>
-
 </body>
 </html>
