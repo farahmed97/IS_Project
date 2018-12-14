81bc1520f39fa76cb11fcd5ca4be83b58855920c
farahmed97 <farah_ahmed1997@yahoo.com>
2018-12-14 18:46:49 +0200

    Add my code

---
+++ mygradingsystem/homepage.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Use title if it's in the page YAML frontmatter -->
+    <title>Welcome to XAMPP</title>
+
+    <meta name="description" content="XAMPP is an easy to install Apache distribution containing MariaDB, PHP and Perl." />
+    <meta name="keywords" content="xampp, apache, php, perl, mariadb, open source distribution" />
+
+    <link href="/mygradingsystem/stylesheets/normalize.css" rel="stylesheet" type="text/css" /><link href="/mygradingsystem/stylesheets/all.css" rel="stylesheet" type="text/css" />
+    <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/3.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+    <script src="/mygradingsystem/javascripts/modernizr.js" type="text/javascript"></script>
+
+
+    <link href="/mygradingsystem/images/favicon.png" rel="icon" type="image/png" />
+
+
+  </head>
+
+  <body class="index">
+    <div id="fb-root"></div>
+    <div class="contain-to-grid">
+      <nav class="top-bar" data-topbar>
+        <ul class="title-area">
+          <li class="name">
+            <h1><a href="/mygradingsystem/homepage.php">Grading System</a></h1>
+          </li>
+          <li class="toggle-topbar menu-icon">
+            <a href="#">
+              <span>Menu</span>
+            </a>
+          </li>
+        </ul>
+
+        <section class="top-bar-section">
+          <!-- Right Nav Section -->
+          <ul class="right">
+              <li class=""><a href="/mygradingsystem/login.php">Login</a></li>
+              <li class=""><a href="/mygradingsystem/viewgrades.php">View Grades</a></li>
+          </ul>
+        </section>
+      </nav>
+    </div>
+
+    <div id="wrapper">
+      <div class="hero">
+  <div class="row">
+    <div class="large-12 columns">
+      <h1><img src="/mygradingsystem/images/A.jpg" />Grading System </h1>
+    </div>
+  </div>
+   </div>
+  </div>
+
+</div>
+
+    <footer>
+      <div class="row">
+        <div class="large-12 columns">
+          <div class="row">
+            <div class="large-8 columns">
+            
+              <ul class="inline-list">
+                <li>Contact Us:</li>
+                <li>E-mail:<a href="https://mail.yahoo.com/d/compose/5582073194">grades@gradingsystem.com</a></li>
+                
+              </ul>
+            </div>
+            <div class="large-4 columns">
+              <p class="text-right">Copyright (c) 2018, Grading System</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- JS Libraries -->
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="/mygradingsystem/javascripts/all.js" type="text/javascript"></script>
+  </body>
+</html>
